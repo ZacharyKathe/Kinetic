982957d85bdcc7f895e7d64261d9e0602828c952
tylercmac <tylercmac@gmail.com>
2021-05-27 14:41:49 -0700

    login works, able to get user info. working on auto redirect to dashboard

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Container from "../components/Container";
 import Col from "../components/Col";
 import Row from "../components/Row";
@@ -7,108 +7,111 @@ import { Link, useLocation } from "react-router-dom";
 import API from '../utils/API'
 import Rotate from 'react-reveal/Rotate'
 
-const Login = () => {
+const Login = ({token}) => {
 
-  const [formState,setFormState] = useState({
-    email:"",
-    password:""
+  const [formState, setFormState] = useState({
+    email: "",
+    password: ""
   })
 
-  const [userState,setUserState] = useState({
-    token:"",
-    user:{
-    }
+  const [userState, setUserState] = useState({
+    token: "",
+    user: {}
   })
 
-  useEffect(()=>{
-    const token = localStorage.getItem("token")
-    if(token){
-      API.getDashboard(token).then(res=>{
+  useEffect(() => {
+    if (token) {
+      API.getProfile(token).then(res => {
         console.log(res.data);
         setUserState({
-          token:token,
-          user:{
-            email:res.data.email,
-            id:res.data.id,
-            username:res.data.username
+          token: token,
+          user: {
+            email: res.data.email,
+            id: res.data.id,
+            username: res.data.username
           }
         })
-      }).catch(err=>{
+        console.log(userState);
+      }).catch(err => {
         console.log("no logged in user", err)
         setUserState({
-          token:"",
-          user:{}
+          token: "",
+          user: {}
         })
       })
     } else {
       console.log("no token provided")
     }
-    
-  },[])
+
+  }, [])
 
   // When user tries to login:
-  const handleFormSubmit = e =>{
+  const handleFormSubmit = e => {
     e.preventDefault();
-    API.login(formState).then(res=>{
+    API.login(formState).then(res => {
       console.log(res.data);
-      localStorage.setItem("token",res.data.token)
+      localStorage.setItem("token", res.data.token)
       setUserState({
         ...userState,
-        token:res.data.token,
-        user:{
-          email:res.data.user.email,
-          name:res.data.user.name,
-          id:res.data.user.id
+        token: res.data.token,
+        user: {
+          email: res.data.user.email,
+          username: res.data.user.username,
+          id: res.data.user.id
         }
       })
-      API.getDashboard(userState.token);
-    }).catch(err=>{
+      localStorage.setItem("user", JSON.stringify(res.data.user))
+      // localStorage.setItem('user', JSON.stringify(userState.user.username))
+
+    }).catch(err => {
       console.log("error occured")
       console.log(err);
       localStorage.removeItem("token");
       setUserState({
-        token:"",
-        user:{}
+        token: "",
+        user: {}
       })
     })
     setFormState({
-      email:"",
-      password:""
+      email: "",
+      password: ""
     })
   }
 
   const location = useLocation();
-  
-  
+
+
 
   return (
     <div className='logInScreen'>
-      <Link to="/NewUser" className={location.pathname === "/NewUser" ? "nav-link active" : "nav-link NewUser"}>
-          New User
+      <Link to="/newuser" className={location.pathname === "/newuser" ? "nav-link active" : "nav-link NewUser"}>
+        New User
         </Link>
-      
+
       <Rotate>
-      <Jump> <h2 className='logIn'>Log in</h2> </Jump>
-      <form onSubmit={handleFormSubmit}>
-        <Container className="signInForm">
-          <Row className="form-group">
-            <Col size="12">
-              <input className="form-control" type="text" placeholder="Email" name="email" value={formState.email} onChange={event=>setFormState({ ...formState, email: event.target.value})} />
-            </Col>
-          </Row>
-          <Row className="form-group">
-            <Col size="12">
-              <input className="form-control"  type="password"  placeholder="Password"  name="password" value={formState.password} onChange={event=>setFormState({ ...formState, password: event.target.value})}
-              />
-            </Col>
-          </Row>
-          <button className="btn btn-success" type="submit">
-            Log in
-          </button>
-        </Container>
-      </form>
+        <Jump> <h2 className='logIn'>Log in</h2> </Jump>
+        <form onSubmit={handleFormSubmit}>
+          <Container className="signInForm">
+            <Row className="form-group">
+              <Col size="12">
+                <input className="form-control" type="text" placeholder="Email" name="email" value={formState.email} onChange={event => setFormState({ ...formState, email: event.target.value })} />
+              </Col>
+            </Row>
+            <Row className="form-group">
+              <Col size="12">
+                <input className="form-control" type="password" placeholder="Password" name="password" value={formState.password} onChange={event => setFormState({ ...formState, password: event.target.value })}
+                />
+              </Col>
+            </Row>
+            {/* <Link to="/dashboard"> */}
+              <button className="btn btn-success" type="submit" >
+                Log in
+              </button>
+            {/* </Link> */}
+          </Container>
+        </form>
       </Rotate>
-      
+
 
     </div>
   );
