e3ab6a4160a55f36e7361a209a2dcc26f8c5d5d8
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-05-31 12:18:47 -0700

    removed old code

--- src/components/GoalDetails/index.js
+++ src/components/GoalDetails/index.js
@@ -44,7 +44,7 @@ export default function GoalDetails(props) {
     };
 
     return (
-      <>
+        <>
         <span onClick={() => setShow(true)} className="remove">
           Details
         </span>
@@ -54,168 +54,20 @@ export default function GoalDetails(props) {
           dialogClassName="modal-90w"
           aria-labelledby="example-custom-modal-styling-title"
         >
-            <form onSubmit={handleEditSubmit} className="create-goal-form">
           <Modal.Header closeButton>
             <Modal.Title id="example-custom-modal-styling-title">
             <Row className="create-goal-row">
               <Col size="12">
-                <input
-                  className="create-goal-input"
-                  type="text"
-                  placeholder=" Enter a name for your goal..."
-                  name="goal_name"
-                  value={editGoal.goal_name}
-                  onChange={(e) => setEditGoal({...editGoal, goal_name: e.target.value})}
-                />
+                <h1 className="goal-details-name">
+                    {editGoal.goal_name}
+                </h1>
               </Col>
             </Row>
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
 
-            <Row className="create-goal-row">
-              <Col size="12">
-                <p className="create-goal-label">Description</p>
-                <input
-                  className="create-goal-input"
-                  type="text"
-                  placeholder=" Describe your goal..."
-                  name="goal_description"
-                  value={editGoal.goal_description}
-                  onChange={(e) => setEditGoal({...editGoal, goal_description: e.target.value})}
-                />
-              </Col>
-            </Row>
-            <Row className="create-goal-row">
-              <Col size="12">
-                <p className="create-goal-label">Goal Category</p>
-                <select
-                  className="create-goal-input"
-                  type="text"
-                  placeholder=" Choose a goal category..."
-                  name="goal_category"
-                  value={editGoal.goal_category}
-                  onChange={(e) => setEditGoal({...editGoal, goal_category: e.target.value})}
-                >
-                  <option value="" disabled defaultValue hidden> </option>
-                  <option value="Diet">Diet</option>
-                  <option value="Education">Education</option>
-                  <option value="Exercise">Exercise</option>
-                  <option value="Financial">Financial</option>
-                  <option value="Habit">Habit</option>
-                  <option value="Health">Health</option>
-                  <option value="Relationship">Relationship</option>
-                  <option value="Work">Work</option>
-                </select>  
-              </Col>
-            </Row>
-            <Row className="create-goal-row">
-              <Col size="12">
-                <p className="create-goal-label">Goal frequency</p>
-                <select
-                  className="create-goal-input"
-                  type="text"
-                  placeholder=" Choose a goal frequency..."
-                  name="goal_frequency"
-                  value={editGoal.goal_frequency}
-                  onChange={(e) => setEditGoal({...editGoal, goal_frequency: e.target.value})}
-                >
-                  <option value="" disabled defaultValue hidden> </option>
-                  <option value="One Time">One Time</option>
-                  <option value="Daily">Daily</option>
-                  <option value="Weekly">Weekly</option>
-                  <option value="Monthly">Monthly</option>
-                  <option value="Yearly">Yearly</option>
-                </select>
-              </Col>
-            </Row>
-            <Row className="create-goal-row">
-              <Col size="12">
-                <p className="create-goal-label">Goal Target</p>
-                <input
-                  className="create-goal-input"
-                  type="number"
-                  placeholder=" Choose a target number for your goal..."
-                  name="goal_target"
-                  value={editGoal.goal_target}
-                  onChange={(e) => setEditGoal({...editGoal, goal_target: e.target.value})}
-                />
-              </Col>
-            </Row>
-            <Row className="create-goal-row">
-              <Col size="12">
-                <p className="create-goal-label">Goal Progress</p>
-                <input
-                  className="create-goal-input"
-                  type="number"
-                  placeholder=" How much have you achieved towards your goal?"
-                  name="goal_target"
-                  value={editGoal.goal_progress}
-                  onChange={(e) => setEditGoal({...editGoal, goal_progress: e.target.value})}
-                />
-              </Col>
-            </Row>
-            <Row className="create-goal-row">
-              <Col size="12">
-                <p className="create-goal-label">Target value type</p>
-                <select
-                  className="create-goal-input"
-                  type="text"
-                  placeholder=" What type of value is your target..."
-                  name="value_type"
-                  value={editGoal.value_type}
-                  onChange={(e) => setEditGoal({...editGoal, value_type: e.target.value})}
-                >
-                  <option value="Other" defaultValue hidden>Other</option>
-                  <option value="Event">Event</option>
-                  <option value="Hours">Hours</option>
-                  <option value="Kilograms">Kilograms</option>
-                  <option value="Liters">Liters</option>
-                  <option value="Miles">Miles</option>
-                  <option value="Minutes">Minutes</option>
-                  <option value="Kilometers">Kilometers</option>
-                  <option value="Pounds">Pounds</option>
-                  {/* <option value="Other">Other</option> */}
-                </select>
-              </Col>
-            </Row>
-            <div className="goal-dates">
-              <div className="actual-dates">
-                <p className="create-goal-label">
-                  Start date
-                </p>
-                <input
-                  className="create-goal-input"
-                  type="date"
-                  name="goal_start"
-                  value={editGoal.goal_start}
-                  onChange={(e) => setEditGoal({...editGoal, goal_start: e.target.value})}
-                />
-              </div>
-              <div className="actual-dates">
-              <p className="create-goal-label">
-                  End date
-                </p>
-                <input
-                  className="create-goal-input"
-                  type="date"
-                  placeholder=" mm/dd/yyyy"
-                  name="goal_finish"
-                  value={editGoal.goal_finish}
-                  onChange={(e) => setEditGoal({...editGoal, goal_finish: e.target.value})}
-                />
-              </div>
-            </div>
-            <Button 
-              variant="outline-primary"
-              size="lg" 
-              block
-              type="submit"
-            >
-              Save
-            </Button>
           </Modal.Body>
-          </form>
         </Modal>
       </>
     );
