2fbec5b7fa93d17e03ab9e3a26f7090f2037c634
tylercmac <tylercmac@gmail.com>
2021-05-31 17:51:33 -0700

    mark complete and clear goal works now, but still doesnt render autopmatically

--- src/components/DashboardCard/index.js
+++ src/components/DashboardCard/index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import API from "../../utils/API";
 // import { useHistory } from "react-router-dom";
 import { ProgressBar, Alert } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Dropdown from "../Dropdown";
 import Moment from "moment";
+import "./style.css"
 
 function DashboardCard(props) {
 
@@ -12,25 +13,29 @@ function DashboardCard(props) {
 
   // useEffect(() =>)
 
-  const markComplete = () => {
+  const checkComplete = () => {
     if (props.goal_target === props.goal_progress) {
       return (
-        <Alert key="success" variant="success" id="success-alert">
+        <Alert key="success" variant="success" className="goal-alert">
           Way to go! 
           <button className="complete-link" onClick={() => {
             const updatedGoal = {
               isComplete: true,
               completedAt: Moment().format("YYYY-MM-DD")
             }
-            API.editGoal(props.id, updatedGoal, localStorage.getItem('token')).then(res => console.log(res))
-          }}>MARK COMPLETE</button>
+            API.editGoal(props.id, updatedGoal, localStorage.getItem('token')).then(res => setTimeout(window.location.reload.bind(window.location), 300))
+          }}>CLEAR GOAL</button>
         </Alert>
       )
     } else return (
-      <Alert key="warning" variant="warning" id="success-alert">
+      <Alert key="warning" variant="warning" className="goal-alert">
         Keep up the good work! 
       </Alert>
     )
+  }
+
+  const markComplete = () => {
+    API.editGoal(props.id, { goal_progress: props.goal_target }, localStorage.getItem('token')).then(res => setTimeout(window.location.reload.bind(window.location), 300))
   }
 
 
@@ -71,7 +76,7 @@ function DashboardCard(props) {
         </div>
 
       </div>
-      {markComplete()}
+      {checkComplete()}
       <ProgressBar now={pctComplete} label={props.value_type === "Event" || props.value_type === "Other" || !props.value_type ? `${props.goal_progress} out of ${props.goal_target} completed!` : `${props.goal_progress} out of ${props.goal_target} ${props.value_type} completed!`} />
     </div>
 
