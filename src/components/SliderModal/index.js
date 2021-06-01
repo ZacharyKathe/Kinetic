9839557070ed940ece32d42fc43dc5c1913b129a
Gushihiro <robinsonhiroto@gmail.com>
2021-05-31 21:48:11 -0700

    changed button to open slider, changed the default font for now

--- src/components/SliderModal/index.js
+++ src/components/SliderModal/index.js
@@ -1,19 +1,23 @@
-import { Modal, Button } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
 import React, { useState } from 'react';
 import EditSlider from '../EditSlider/index';
 import addBtn from '../../images/add-btn.png';
+import IconButton from '@material-ui/core/IconButton';
+import EditAttributesIcon from '@material-ui/icons/EditAttributes';
 
 export default function SliderModal(props) {
   const [show, setShow] = useState(false);
 
   return (
     <>
-      <Button 
+      <IconButton 
         onClick={() => setShow(true)} 
-        variant="primary"
+        color="primary"
       >
-        <img src={addBtn} alt=""/>
-      </Button>
+        <EditAttributesIcon
+          fontSize="large"
+        />
+      </IconButton>
       <Modal
         show={show}
         onHide={() => setShow(false)}
