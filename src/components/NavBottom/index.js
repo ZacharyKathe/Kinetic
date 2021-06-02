23ad04e7c65300b09d51903719dd0fa47a6d7405
Gushihiro <robinsonhiroto@gmail.com>
2021-06-02 04:39:20 -0700

    separated navtabs into different pages

--- src/components/NavBottom/index.js
+++ src/components/NavBottom/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./style.css";
+import { useHistory } from 'react-router-dom';
+
 
 function NavBottom(props) {
+    const history = useHistory();
     return (
         <div className="bottom-nav">
-            <div onClick={() => props.setSelectedTab('My Goals')}><img src={props.homeBtn} alt="home button" /></div>
-            <div onClick={() => props.setSelectedTab('My Groups')}><img src={props.groupsBtn} alt="groups button" /></div>
-            <div onClick={() => props.setSelectedTab('Calendar')}><img src={props.calendarBtn} alt="calendar button" /></div>
+            <div onClick={() => history.push('/dashboard')}><img src={props.homeBtn} alt="home button" /></div>
+            <div onClick={() => history.push('/dashboard/mygroups')}><img src={props.groupsBtn} alt="groups button" /></div>
+            <div onClick={() => history.push('/dashboard/mycalendar')}><img src={props.calendarBtn} alt="calendar button" /></div>
         </div>
     )
 }
