1b79892a56ac19b897e13d83d7409568662034bd
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-06-02 11:32:51 -0700

    created initial desktop components

---
+++ src/components/DesktopNav/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import backBtn from "../../images/back.png";
+import exit from "../../images/exit.png";
+import "./style.css";
+import { useHistory } from "react-router-dom";
+
+function DesktopNav(props) {
+
+    const history = useHistory();
+    
+    return (
+        
+        <div className="top-nav" >
+            <img src={backBtn} alt="back button" onClick={() => history.goBack()}/>
+            <p className="header-text">{props.header}</p>
+            <img src={exit} alt="log out button" onClick={()=> window.localStorage.clear()+ window.location.reload()}/>
+        </div>
+    )
+}
+
+export default DesktopNav;
