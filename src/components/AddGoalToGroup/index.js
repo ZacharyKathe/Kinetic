7e678aae1cd45bb6404d6ee79422cb93e47986de
tylercmac <tylercmac@gmail.com>
2021-06-05 15:13:28 -0700

    saving for pull

--- src/components/AddGoalToGroup/index.js
+++ src/components/AddGoalToGroup/index.js
@@ -1,13 +1,53 @@
-import React from 'react';
-// import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 import { Modal } from 'react-bootstrap';
 import API from '../../utils/API';
-import Col from '../Col/index';
-import Row from '../Row/index';
+import Moment from "moment";
+import renderActivityIcon from '../renderCategoryIcon'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 
 export default function AddGoalToGroup(props) {
 
+  const [goalsInGroup, setGoalsInGroup] = useState([])
+
   // const history = useHistory();
+  // let tempGoalsArr = [];
+
+  // const goalArr = []
+  useEffect(() => {
+    // setGoalsInGroup([])
+    console.log(props.group_id);
+    if (props.goals) {
+      props.goals.map(goal => {
+        console.log(goal);
+        goal.Groups.map(group => {
+          console.log(group.id);
+          if (Number(props.group_id) === group.id) {
+            console.log(goal);
+            setGoalsInGroup(goalsInGroup => [...goalsInGroup, goal])
+          }
+        })
+      });
+    }
+
+  }, [])
+
+
+  const renderGroupName = (goal) => {
+    if (goalsInGroup) {
+      for (const groupGoal of goalsInGroup) {
+        console.log(goal.id);
+        console.log(groupGoal.id);
+        console.log(groupGoal);
+        if (goal.id === groupGoal.id) {
+          console.log(props.thisGroup);
+          return props.thisGroup.name;
+        } else return false;
+      }
+    }
+  }
 
   const handleGoalAdd = (goalID) => {
     const token = localStorage.getItem('token');
@@ -17,7 +57,6 @@ export default function AddGoalToGroup(props) {
     console.log(goalObj);
     API.addGoalToGroup(props.group_id, goalObj, token)
       .then(res => {
-        
         props.updateGoals();
         props.setModalShow(false);
       })
@@ -37,22 +76,34 @@ export default function AddGoalToGroup(props) {
       >
         <Modal.Header closeButton>
           <Modal.Title id="goals-to-choose">
-            <h1>Share which goal?</h1>
+            <h2>Share which goal?</h2>
           </Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
-          {props.goals ? props.goals.map((goal => {
-            return (
-              <Row className="create-goal-row" key={goal.id}>
-                <Col size="12">
-                  <button onClick={() => handleGoalAdd(goal.id)}>{goal.goal_name}</button>
-                </Col>
-              </Row>
-            )
-          })) :
-            console.log('no goals')
-          }
+          <List>
+            {props.goals ? props.goals.map((goal => {
+              return (
+                <div key={goal.id}>
+                  <div className="border-top" />
+                  <a href="#" onClick={() => handleGoalAdd(goal.id)}>
+                    <ListItem>
+                      <ListItemIcon>
+                        {renderActivityIcon(goal.goal_category)}
+                      </ListItemIcon>
+                      <ListItemText
+                        primary={`${goal.goal_name} ${renderGroupName(goal) ? `(already a part of ${renderGroupName(goal)})` : ""}`}
+                        secondary={goal.lastUpdate ? `Last Updated: ${Moment(goal.lastUpdate).format("MMMM Do hh:mma")}` : ""}
+                        // secondary={renderGroupName(goal) ? `already a part of ${renderGroupName(goal)}` : ""}
+                      />
+                    </ListItem>
+                  </a>
+                </div>
+              )
+            })) :
+              console.log('no goals')
+            }
+          </List>
         </Modal.Body>
 
       </Modal>
