1027608d14c2392bfa4b28c208447670140c0857
Gushihiro <robinsonhiroto@gmail.com>
2021-06-06 20:06:12 -0700

    stf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import React, { useState, useEffect } from "react";
 import updateGoals from "./components/updateGoals"
 import API from "./utils/API";
-import React, { useState, useEffect } from "react";
 import Group from './pages/Group';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Login from "./pages/Login";
